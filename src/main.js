9f51ac154aec6f7346bb5899e5843b59d13ab553
Franco Durand <francoduve.25@gmail.com>
2024-09-19 00:16:36 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import i18n from './i18n';
+
+const app = createApp(App);
+
+app.use(i18n);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+});
+
+app.mount('#app')
