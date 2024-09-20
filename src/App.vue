993f2d0c76881594f61b5d7a765b617856580325
Daniel-10806 <U202211108@upc.edu.pe>
2024-09-20 16:32:38 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <div id="app">
-    <MessagePanel />
-  </div>
+
 </template>
 
 <script>
-import MessagePanel from './components/messaging/MessagePanel.vue';
 
-export default {
-  components: {
-    MessagePanel,
-  },
-};
 </script>
 
 <style>
