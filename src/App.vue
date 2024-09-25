d061b8ab5035aa6f1190cb015b74b5b518f388c6
Franco Durand <francoduve.25@gmail.com>
2024-09-24 19:54:47 -0500

    feat: add toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
+  <toolbar></toolbar>
   <router-view />
 </template>
 
 <script>
+import toolbar from './public/components/Toolbar.vue'
+
+export default {
+  components: {
+    toolbar
+  }
+}
 </script>
 
 <style></style>
